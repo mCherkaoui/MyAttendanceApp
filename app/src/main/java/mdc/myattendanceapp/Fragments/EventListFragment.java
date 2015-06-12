64736be5b001149e81a334139e0dee31facfeb62
mCherkaoui <darif_marouane@yahoo.fr>
2015-06-13 00:01:00 +0200

    Initial commit

---
+++ app/src/main/java/mdc/myattendanceapp/Fragments/EventListFragment.java
@@ -0,0 +1,31 @@
+package mdc.myattendanceapp.Fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mdc.myattendanceapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventListFragment extends Fragment {
+
+
+    public EventListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank_fragment2, container, false);
+    }
+
+
+}
