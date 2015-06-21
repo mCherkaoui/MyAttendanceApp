6e1238a8d6f636b3cdec9e024069bf3adcab6bd2
mCherkaoui <darif_marouane@yahoo.fr>
2015-06-21 20:48:37 +0200

    ToolBar&Layout

--- app/src/main/java/mdc/myattendanceapp/Activities/MainActivity.java
+++ app/src/main/java/mdc/myattendanceapp/Activities/MainActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+
 
 import mdc.myattendanceapp.R;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
     }
 
 
